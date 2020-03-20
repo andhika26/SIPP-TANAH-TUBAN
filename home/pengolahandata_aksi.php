71aef0dddc8c83e9aede72cb2d477705ac72c19e
Andhika Nur Afian <andhika.na@gmail.com>
2020-03-20 21:44:28 +0700

    SIPP TUBAN

--- home/pengolahandata_aksi.php
+++ home/pengolahandata_aksi.php
@@ -27,7 +27,7 @@ if (isset($_POST['simpan'])) {
 		$status = '1';
      
         // SQL query untuk memeriksa apakah users terdapat di database?
-        $query = mysqli_query($koneksi,"UPDATE tbl_olahdata petugas='$petugaslog', ke_bon='$kepetugasbon', dari_bon='$daripetugasbon', seri_bon='$seribon', petugas_gambar='$petugasgambar', ke_ptgsgambar='$kepetugasgambar', dr_ptgsgambar='$daripetugasgambar', dr_petugasukur='$daripetugasukur', ke_pemetaan='$kepemetaan', dr_pemetaan='$daripemetaan', status='$status', updated='$update_at' SET WHERE DI302='$no302' ");  
+        $query = mysqli_query($koneksi,"UPDATE tbl_olahdata SET petugas='$petugaslog', ke_bon='$kepetugasbon', dari_bon='$daripetugasbon', seri_bon='$seribon', petugas_gambar='$petugasgambar', ke_ptgsgambar='$kepetugasgambar', dr_ptgsgambar='$daripetugasgambar', dr_petugasukur='$daripetugasukur', ke_pemetaan='$kepemetaan', dr_pemetaan='$daripemetaan', status='$status', updated='$update_at'  WHERE DI302='$no302' ");  
 
 		//$query = mysqli_query($koneksi,"INSERT INTO tbl_ukur VALUES ('', '$noberkas','$di302', '', '', '', '', '', '', '', '', '0')");  
 		
